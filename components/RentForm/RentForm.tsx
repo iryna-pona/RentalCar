f0f42bb93748976889de05642b5eaba7fb1ebb83
iryna-pona <ipona3710@gmail.com>
2026-03-10 15:56:06 +0200

    added RentForm

--- components/RentForm/RentForm.tsx
+++ components/RentForm/RentForm.tsx
@@ -1,13 +1,56 @@
+"use client";
+
+import { Formik, Form, Field, ErrorMessage, FormikHelpers } from "formik";
+import * as Yup from "yup";
+import styles from "./RentForm.module.css";
+
+interface RentFormValues {
+  username: string;
+  email: string;
+  bookingDate: string;
+  message: string;
+}
+
+const initialValues: RentFormValues = {
+  username: "",
+  email: "",
+  bookingDate: "",
+  message: "",
+};
+
+const RentFormSchema = Yup.object().shape({
+  username: Yup.string()
+    .min(2, "Name must be at least 2 characters")
+    .max(30, "Name is too long")
+    .required("Name is required"),
+  email: Yup.string()
+    .email("Invalid email format")
+    .required("Email is required"),
+});
+
 export default function RentForm() {
+    const handleSubmit = (
+    values: RentFormValues,
+    actions: FormikHelpers<RentFormValues>
+  ) => {
+    actions.resetForm();
+  };
     return (
         <div>
             <h2>Book your car now</h2>
             <p>Stay connected! We are always ready to help you.</p>
-            <button
-                className={styles.buttonRent}
-                type="submit"
-                disabled={isSubmitting}
-            >Send</button>
+            <Formik initialValues={initialValues} validationSchema={RentFormSchema} onSubmit={handleSubmit}>
+                <Form>
+                    <Field type="text" name="username" placeholder = 'Name*' />
+                    <Field type="email" name="email" placeholder = 'Email*' />
+                    <Field type="date" name="bookingDate" placeholder = 'Booking date' />
+                    <Field as="textarea" name="message" id="message" rows={3} placeholder = 'Comment' />
+                    <button
+                        className={styles.buttonRent}
+                        type="submit"
+                    >Send</button>
+                </Form>
+            </Formik>           
         </div>
     )
 }
