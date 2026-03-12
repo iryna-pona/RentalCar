814872ac9db1c8baa4ce17af5965b6fd35dbac4d
iryna-pona <ipona3710@gmail.com>
2026-03-12 22:08:33 +0200

    added page Home, layout, update Image to CarDetails

--- components/CarDetails/CarDetails.tsx
+++ components/CarDetails/CarDetails.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import Image from "next/image";
 import { Car } from "@/types/car";
 import styles from "./CarDetails.module.css";
 
@@ -5,60 +8,95 @@ interface CarDetailsProps {
   car: Car;
 }
 
-const Icon = () => (
-  <span className={styles.icon}>✔️</span> // або свій SVG/спрайт
+const CheckIcon = () => (
+  <Image
+    src="/icons/check-circle.svg"
+    alt="Check"
+    width={24}
+    height={24}
+    className={styles.icon}
+  />
 );
 
 export default function CarDetails({ car }: CarDetailsProps) {
-    return (
-        <div className={styles.carDeteils}>
-            <h2>{car.brand} {car.model}, {car.year}</h2>
-            <p>id: {car.id}</p>
-            <p>{car.address}</p>
-            <p>Mileage: {car.mileage.toLocaleString()} km</p>
-            <p>${car.rentalPrice}</p>
-            <p>{car.description}</p>
-            
-            {/* Rental Conditions */}
-            <h3>Rental Conditions:</h3>
-            <ul className={styles.list}>
-              {car.rentalConditions.map((c, i) => (
-                <li key={i}>
-                  <Icon /> {c}
-                </li>
-              ))}
-            </ul>
+  return (
+    <div className={styles.carDetails}>
+      <h2>
+        {car.brand} {car.model}, {car.year}
+      </h2>
 
-            {/* Car Specifications */}
-            <h3>Car Specifications:</h3>
-            <ul className={styles.list}>
-              <li>
-                <img src="/icons/year.svg" alt="Year" className={styles.icon} />
-                Year: {car.year}
-              </li>
-              <li>
-                <img src="/icons/type.svg" alt="Type" className={styles.icon} />
-                Type: {car.type}
-              </li>
-              <li>
-                <img src="/icons/fuel.svg" alt="Fuel" className={styles.icon} />
-                Fuel Consumption: {car.fuelConsumption}
-              </li>
-              <li>
-                <img src="/icons/engine.svg" alt="Engine" className={styles.icon} />
-                Engine Size: {car.engineSize}
-              </li>
-            </ul>
+      <p>id: {car.id}</p>
+      <p>{car.address}</p>
+      <p>Mileage: {car.mileage.toLocaleString()} km</p>
+      <p>${car.rentalPrice}</p>
+      <p>{car.description}</p>
 
-            {/* Accessories and Functionalities */}
-            <h3>Accessories and Functionalities:</h3>
-            <ul className={styles.list}>
-              {[...car.accessories, ...car.functionalities].map((a, i) => (
-                <li key={i}>
-                  <Icon /> {a}
-                </li>
-              ))}
-            </ul>
-        </div>
-    );
+      <h3>Rental Conditions:</h3>
+      <ul className={styles.list}>
+        {car.rentalConditions.map((c, i) => (
+          <li key={i}>
+            <CheckIcon />
+            {c}
+          </li>
+        ))}
+      </ul>
+
+      <h3>Car Specifications:</h3>
+      <ul className={styles.list}>
+        <li>
+          <Image
+            src="/icons/year.svg"
+            alt="Year"
+            width={24}
+            height={24}
+            className={styles.icon}
+          />
+          Year: {car.year}
+        </li>
+
+        <li>
+          <Image
+            src="/icons/type.svg"
+            alt="Type"
+            width={24}
+            height={24}
+            className={styles.icon}
+          />
+          Type: {car.type}
+        </li>
+
+        <li>
+          <Image
+            src="/icons/fuel.svg"
+            alt="Fuel"
+            width={24}
+            height={24}
+            className={styles.icon}
+          />
+          Fuel Consumption: {car.fuelConsumption}
+        </li>
+
+        <li>
+          <Image
+            src="/icons/engine.svg"
+            alt="Engine"
+            width={24}
+            height={24}
+            className={styles.icon}
+          />
+          Engine Size: {car.engineSize}
+        </li>
+      </ul>
+
+      <h3>Accessories and Functionalities:</h3>
+      <ul className={styles.list}>
+        {[...car.accessories, ...car.functionalities].map((a, i) => (
+          <li key={i}>
+            <CheckIcon />
+            {a}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
